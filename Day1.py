a76a798bb2d93e79eac07956ba19ed96d2e902e1
krism117 <k.murchington@googlemail.com>
2020-11-14 21:08:51 +0000

    test

--- Day1.py
+++ Day1.py
@@ -1,1 +1,12 @@
-print('Hello world')
+# band name generator
+# Create a greeting for your program
+print("Hello and welcome to band name maker")
+# Ask the user for the city they grew up in
+city = input("What city did you grow up in?\n")
+# Ask the user for the name of a pet
+pet = input('What is the name of a pet?\n')
+# Combine the name of their city and pet
+
+
+print("Your band name is " + city + " " + pet)
+
