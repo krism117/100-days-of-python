7c7356d9c60c03bfb769e3a7ae39f64f79a563f9
krism117 <k.murchington@googlemail.com>
2020-11-15 13:29:26 +0000

    test

---
+++ Day 2/Day2.py
@@ -0,0 +1,16 @@
+#two digit input
+
+two_digit_input = input("Type a two digit number: ")
+
+first_number = two_digit_input[0]
+second_number = two_digit_input[1]
+
+first_number_int = int(first_number)
+second_number_int = int(second_number)
+
+result = first_number_int + second_number_int
+print (result)
+
+# print(type(two_digit_input))
+# print(first_number)
+
